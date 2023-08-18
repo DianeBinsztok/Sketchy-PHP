d1c06d2ead2c9bf3f537f1f76732399689ba2a8b
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-08-18 14:03:22 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Galerie</title>
+</head>
+<body>
+    <header>Ma Galerie</header>
+    <nav>
+        <ul>
+            <li><a href="/">accueil</a></li>
+            <li><a href="/croquis">Croquis</a></li>
+            <li><a href="/nu">Nu</a></li>
+            <li><a href="/voyage">Carnets de voyage</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h1>Le titre de la page</h1>
+        <section>Une barre de filtres?</section>
+        <section>Une boucle PHP avec les miniatures d'images</section>
+        <section>Une autre boucle avec les modales associées</section>
+    </main>
+    <footer>Diane 2023</footer>
+    
+</body>
+</html>
